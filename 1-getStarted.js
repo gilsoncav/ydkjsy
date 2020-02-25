30856a72e9db1224bedc8ce3f48947c08e4f6b09
Gilson Cavalcanti <gilsoncav@gmail.com>
2020-02-25 09:10:34 -0300

    testing a new object with methods in a odd way

---
+++ 1-getStarted.js
@@ -0,0 +1,16 @@
+let whatToSay = {
+    greeting() {
+        console.log('Hi you all!');
+    },
+    question() {
+        console.log('What\'s the meaning of the life?');
+    },
+    answer() {
+        console.log('42');
+    }
+};
+
+whatToSay.greeting();
+
+whatToSay.answer();
+
